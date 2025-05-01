e72e5cb9aadbf22f20684f2a2b8c17b72827faac
Shuo Cheng Hu <shuochenghu@gmail.com>
2025-05-01 13:00:08 +0800

    2025_05_01

---
+++ 2025_05_01/Tutorial 8-1/Password Validation/Password Validation/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Password_Validation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.checkPasswordButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.Location = new System.Drawing.Point(21, 9);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(243, 93);
+            this.instructionsLabel.TabIndex = 0;
+            this.instructionsLabel.Text = "請輸入一個密碼，密碼必須包含：\n- 至少8個字元\n- 至少一個大寫字母\n- 至少一個小寫字母\n- 至少一個數字";
+            this.instructionsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(76, 108);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(132, 16);
+            this.promptLabel.TabIndex = 1;
+            this.promptLabel.Text = "請輸入一個密碼。";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(92, 135);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // checkPasswordButton
+            // 
+            this.checkPasswordButton.Location = new System.Drawing.Point(64, 169);
+            this.checkPasswordButton.Name = "checkPasswordButton";
+            this.checkPasswordButton.Size = new System.Drawing.Size(75, 36);
+            this.checkPasswordButton.TabIndex = 3;
+            this.checkPasswordButton.Text = "檢查密碼";
+            this.checkPasswordButton.UseVisualStyleBackColor = true;
+            this.checkPasswordButton.Click += new System.EventHandler(this.checkPasswordButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(145, 169);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 36);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "退出";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 218);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkPasswordButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.instructionsLabel);
+            this.Name = "Form1";
+            this.Text = "密碼驗證";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button checkPasswordButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 2025_05_01/Tutorial 8-1/Password Validation/Password Validation/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Validation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // NumberUpperCase 方法接受一個字串參數，
+        // 並回傳該字串中包含的大寫字母數量。
+        private int NumberUpperCase(string str)
+        {
+            int upperCaseCount = 0; // 計數器，初始值為0
+            foreach (char c in str)
+            {
+                // 如果字元是大寫字母，則計數器加1
+                if (char.IsUpper(c))
+                {
+                    upperCaseCount++;
+                }
+            }
+            return upperCaseCount; // 回傳大寫字母的數量
+        }
+
+        // NumberLowerCase 方法接受一個字串參數，
+        // 並回傳該字串中包含的小寫字母數量。
+        private int NumberLowerCase(string str)
+        {
+            int lowerCaseCount = 0; // 計數器，初始值為0
+            for (int i = 0; i < str.Length; i++)
+            {
+                // 如果字元是小寫字母，則計數器加1
+                if (char.IsLower(str[i]))
+                {
+                    lowerCaseCount++;
+                }
+            }
+            return lowerCaseCount; // 回傳小寫字母的數量
+        }
+
+        // NumberDigits 方法接受一個字串參數，
+        // 並回傳該字串中包含的數字字元數量。
+        private int NumberDigits(string str)
+        {
+            int digits = 0;
+            foreach (char c in str)
+            {
+                // 如果字元是數字，則計數器加1
+                if (char.IsDigit(c))
+                {
+                    digits++;
+                }
+            }
+            return digits; // 回傳數字字元的數量
+        }
+
+        // 當使用者按下「檢查密碼」按鈕時觸發此事件。
+        // 此方法應該檢查使用者輸入的密碼是否符合規則，
+        // 包括是否包含至少一個大寫字母、一個小寫字母和一個數字。
+        private void checkPasswordButton_Click(object sender, EventArgs e)
+        {
+            const int MIN_LENGTH = 8; // 密碼的最小長度
+            string password = passwordTextBox.Text; // 取得使用者輸入的密碼
+            if ( password.Length >= MIN_LENGTH  && 
+                    NumberUpperCase(password) > 0 &&
+                    NumberLowerCase(password) > 0 &&
+                    NumberDigits(password) > 0)
+            {
+                MessageBox.Show("密碼有效！", "密碼檢查結果", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("密碼無效！", "密碼檢查結果", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 當使用者按下「退出」按鈕時觸發此事件。
+        // 此方法會關閉目前的表單，結束應用程式。
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
